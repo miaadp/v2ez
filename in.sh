97ee2264c56b2f624d94c2a8e0fa1ec02e239deb
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-12-09 16:17:23 +0330

    Update in.sh

--- in.sh
+++ in.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-yes | apt update
+apt -y update
 wget https://raw.githubusercontent.com/miaadp/v2ez/main/install.sh
 chmod +x install.sh
+sleep 1
 bash install.sh
 apt install sudo
 systemctl restart v2ray && systemctl restart nginx
